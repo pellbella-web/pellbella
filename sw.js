3cce024896150ed1e78db5e0cf6bc009c2945379
pellbella-web <pellbellafranqueses@gmail.com>
2026-09-07 18:48:30 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Service worker v2 — limpia todo y se elimina
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
-    .then(()=>self.registration.unregister())
-  );
-  self.clients.claim();
-});
